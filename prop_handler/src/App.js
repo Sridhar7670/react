f596903c398a6ccc4a31ab75d8bdab39cefa8d69
sridhar7670 <sridharnani080@gmail.com>
2025-05-08 21:03:28 +0530

    added other functions

--- prop_handler/src/App.js
+++ prop_handler/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import App1 from "./App1"; // Import App1
 import App2 from "./App2";
 import App3 from "./App3";
+import App4 from "./App4";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <App2/>
       <App1 /> 
       <App3/>
+      <App4/>
     </div>
   );
 }
---
+++ prop_handler/src/App4.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+
+function App4(){
+    const [temp,settemp]=useState("")
+    const[a,seta]=useState([])
+    function fun(e){
+    settemp(e.target.value)
+    }
+    function fun1(){
+        seta([...a,temp])
+        settemp("")
+    }
+    function fun2(index){
+        const newitems=a.filter((i)=>i !== index)
+        seta(newitems)
+    }
+    return(
+       <div>
+         <input onChange={fun} value={temp}/>
+         <button onClick={fun1}>Add</button>
+         {
+         a.length>0 && a.map((i,ind)=>{
+            return (
+                    <div key={ind}>
+                        <div>{i}</div>
+                        <button onClick={()=>fun2(i)}>Delete</button>
+                    </div>
+         )
+                                    }
+        ) 
+        
+        }
+       </div>
+    )
+}
+export default App4
