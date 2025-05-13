041055cb11540e0d935bfd4eb97a1fd00fc8d7c5
sridhar7670 <sridharnani080@gmail.com>
2025-05-13 08:45:16 +0530

    deployment needs

--- MoviesAPK/Back_End/index.js
+++ MoviesAPK/Back_End/index.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 const express = require("express")
 const cors = require("cors")
 const manual_route = require("./routes/route")
-
+const port = process.env.PORT || 4000 
 mongoose.connect("mongodb://127.0.0.1:27017/MoviesAPK").then(()=>{
     console.log("Connection OK")
 }).catch((err)=>console.log(err))
@@ -12,4 +12,4 @@ app.use(express.json())
 app.use(cors())
 app.use("/",manual_route)
 
-app.listen(5000)
+app.listen(port)
