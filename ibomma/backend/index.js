1b60e184d1da010c571575f18610215061bb9450
sridhar7670 <sridharnani080@gmail.com>
2025-05-23 16:57:55 +0530

    vercel pushed

--- ibomma/backend/index.js
+++ ibomma/backend/index.js
@@ -10,9 +10,10 @@ mongoose.connect("mongodb+srv://sridharnani090:ZjCHx8lLESXbqbSq@moviesapk.x9vm7y
 const app = express()
 app.use(express.json())
 app.use(cors({
-  origin: "https://react-li9a.vercel.app/",
+  origin: ["https://react-li9a.vercel.app", "http://localhost:3000"],
   credentials: true
 }))
+
 app.use("/",manual_route)
 
 app.listen(5000)  //vercel doesn't listen 
