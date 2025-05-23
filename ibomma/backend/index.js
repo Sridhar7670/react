20591136b242949d1583d6459abf7a29b5cb9984
sridhar7670 <sridharnani080@gmail.com>
2025-05-23 17:23:46 +0530

    vercel pushed

--- ibomma/backend/index.js
+++ ibomma/backend/index.js
@@ -26,10 +26,13 @@ const manual_route = require("./routes/routes");
 const app = express();
 
 // --- CORS setup ---
-const corsOptions = {
+const cors = require("cors");
+
+app.use(cors({
   origin: ["https://react-li9a.vercel.app", "http://localhost:3000"],
-  credentials: true,
-};
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 
 app.use(cors(corsOptions));
 
