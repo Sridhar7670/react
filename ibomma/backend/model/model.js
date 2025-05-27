48b340b15e97e5fd996ba68cbd8d54b06e1f982f
sridhar7670 <sridharnani080@gmail.com>
2025-05-27 16:45:05 +0530

    model modified

--- ibomma/backend/model/model.js
+++ ibomma/backend/model/model.js
@@ -1,46 +1,28 @@
 const mongoose = require('mongoose');
 
 const movieSchema = new mongoose.Schema({
-  movieName: {
-    type: String,
-    required: true
-  },
-  director: {
-    type: String,
-    required: true
-  },
-  cast: {
-    type: [String],
-    default: []
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  ratingIMDB: {
-    type: String,
-    required: true
-  },
-  img: {
-    type: String,
-    required: true
-  },
-  genere: {
-    type: String,
-    required: true
-  },
-  releaseYear: {
-    type: Number,
-    required: true
-  },
-  language: {
-    type: String,
-    required: true
-  },
-  age: {
-    type: Number,
-    required: false
-  }
+  movieName: {type: String,required: true},
+  director: {type: String,required: true},
+  cast: { type: [String], default: [] },
+  description: { type: String,required: true},
+  ratingIMDB: {type: String,required: true },
+  img: {type: String,required: true},
+  genere: { type: String,required: true },
+  releaseYear: {type: Number,required: true },
+  language: {type: String,required: true},
+  age: {type: Number,required: false},
+  favourites: [{
+    movieName: String,
+    director: String,
+    cast: [String],
+    description: String,
+    ratingIMDB: String,
+    img: String,
+    genere: String,
+    releaseYear: Number,
+    language: String,
+    age: Number
+  }]
 }
 );
 
