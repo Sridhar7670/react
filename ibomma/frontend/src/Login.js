c37e835626f086234d9d53e7246bf99d24afda3a
sridhar7670 <sridharnani080@gmail.com>
2025-05-30 10:25:15 +0530

    login and reg validation at client side

--- ibomma/frontend/src/Login.js
+++ ibomma/frontend/src/Login.js
@@ -6,64 +6,142 @@ import Context from "./Context";
 import Cookies from "js-cookie"
 
 
+// function Login() {
+//   const {setActive}=useContext(Context)
+//   const [message, setMessage] = useState("");
+//   const [a,setA]=useState(false)
+//   const [temp, setTemp] = useState({ _id: "", password: "" });
+//   const navigate=useNavigate()
+//   function Fun(e) {
+//     setTemp({ ...temp, [e.target.name]: e.target.value });
+//   }
+
+//   const Fun1=async()=> {
+//     axios
+//       .post(`https://react-8ypw.vercel.app/login`, temp)
+//       .then((res) => {
+//       setMessage(res.data.msg);
+
+//       if (res.data.token) {
+//         const sendtoken= JSON.stringify(res.data.token)
+//         Cookies.set('token',sendtoken,{expires:1})
+//         setA(true);
+//         setTimeout(() => {
+//           navigate("/");
+//           setActive("home");
+//         }, 1000);
+//       }
+//         })
+//       .catch((err) => console.log(err.message));
+//   }
+
+//   return (
+//     <div className="login">
+//       <h1>Please Login</h1>
+//       <input
+//         type="text"
+//         placeholder="Enter Mail id"
+//         onChange={Fun}
+//         name="_id"
+//         value={temp._id}
+//       />
+//       <input
+//         type="password"
+//         placeholder="Enter password"
+//         onChange={Fun}
+//         name="password"
+//         value={temp.password}
+//       />
+//       {message && <div className={a ? "success-msg" : "error-msg"}>{message}</div>}
+//       <button onClick={Fun1}>Login</button>
+
+//       {/* Sign Up Link */}
+//       <p className="signup-text">
+//         Don't have an account?{" "}
+//         <Link to="/register" className="signup-link" >
+//           Sign up
+//         </Link>
+//       </p>
+//     </div>
+//   );
+// }
 function Login() {
-  const {setActive}=useContext(Context)
+  const { setActive } = useContext(Context);
   const [message, setMessage] = useState("");
-  const [a,setA]=useState(false)
+  const [a, setA] = useState(false);
   const [temp, setTemp] = useState({ _id: "", password: "" });
-  const navigate=useNavigate()
+  const [errors, setErrors] = useState({});
+  const navigate = useNavigate();
+
+  function validate() {
+    const errs = {};
+    if (!temp._id.includes("@")) {
+      errs._id = "Invalid email format";
+    }
+    if (temp.password.length < 6) {
+      errs.password = "Password must be at least 6 characters";
+    }
+    return errs;
+  }
+
   function Fun(e) {
     setTemp({ ...temp, [e.target.name]: e.target.value });
   }
 
-  const Fun1=async()=> {
-    axios
-      .post(`https://react-8ypw.vercel.app/login`, temp)
-      .then((res) => {
-      setMessage(res.data.msg);
+  const Fun1 = async () => {
+    const errs = validate();
+    setErrors(errs);
+    if (Object.keys(errs).length > 0) return;
 
+    try {
+      const res = await axios.post(`https://react-8ypw.vercel.app/login`, temp);
+      setMessage(res.data.msg);
       if (res.data.token) {
-        const sendtoken= JSON.stringify(res.data.token)
-        Cookies.set('token',sendtoken,{expires:1})
+        Cookies.set("token", JSON.stringify(res.data.token), { expires: 1 });
         setA(true);
         setTimeout(() => {
           navigate("/");
           setActive("home");
         }, 1000);
       }
-        })
-      .catch((err) => console.log(err.message));
-  }
+    } catch (err) {
+      console.log(err.message);
+    }
+  };
 
   return (
     <div className="login">
       <h1>Please Login</h1>
+      <label htmlFor="email" className="label-login">Email ID</label>
       <input
         type="text"
-        placeholder="Enter Mail id"
-        onChange={Fun}
+        placeholder="Enter Mail ID"
         name="_id"
+        id="email"
         value={temp._id}
+        onChange={Fun}
       />
+      {errors._id && <p className="error_login">{errors._id}</p>}
+      <label htmlFor="password" className="label-login">Password</label>
       <input
         type="password"
-        placeholder="Enter password"
-        onChange={Fun}
+        placeholder="Enter Password"
         name="password"
+        id="password"
         value={temp.password}
+        onChange={Fun}
       />
+      {errors.password && <p className="error_login">{errors.password}</p>}
+
       {message && <div className={a ? "success-msg" : "error-msg"}>{message}</div>}
       <button onClick={Fun1}>Login</button>
 
-      {/* Sign Up Link */}
       <p className="signup-text">
-        Don't have an account?{" "}
-        <Link to="/register" className="signup-link" >
-          Sign up
-        </Link>
+        Don't have an account? <Link to="/register" className="signup-link">Sign up</Link>
       </p>
     </div>
   );
 }
+
 
 export default React.memo(Login);
--- ibomma/frontend/src/Register.js
+++ ibomma/frontend/src/Register.js
@@ -1,22 +1,49 @@
+import React, { useState } from "react";
 import axios from "axios";
-import { useState } from "react";
-import { Link } from "react-router-dom"; // 👈 For navigation
+import { Link } from "react-router-dom";
 
 const Register = () => {
   const [temp, setTemp] = useState({ _id: "", password: "", name: "" });
   const [message, setMessage] = useState("");
   const [isError, setIsError] = useState(false);
+  const [errors, setErrors] = useState({});
 
   function handleChange(e) {
     setTemp({ ...temp, [e.target.name]: e.target.value });
   }
 
+  function validateForm() {
+    const newErrors = {};
+
+    if (!temp._id.trim()) {
+      newErrors._id = "Email is required";
+    } else if (!/\S+@\S+\.\S+/.test(temp._id)) {
+      newErrors._id = "Invalid email format";
+    }
+
+    if (!temp.name.trim()) {
+      newErrors.name = "Name is required";
+    }
+
+    if (!temp.password.trim()) {
+      newErrors.password = "Password is required";
+    } else if (temp.password.length < 6) {
+      newErrors.password = "Password must be at least 6 characters";
+    }
+
+    setErrors(newErrors);
+    return Object.keys(newErrors).length === 0;
+  }
+
   function handleRegister() {
+    if (!validateForm()) return;
+
     axios
       .post("https://react-8ypw.vercel.app/register", temp)
       .then((res) => {
         setMessage(res.data.msg);
         setIsError(false);
+        setErrors({});
       })
       .catch((err) => {
         if (err.response && err.response.data.msg) {
@@ -32,27 +59,39 @@ const Register = () => {
   return (
     <div className="register">
       <h1>Please Register</h1>
+
+      <label htmlFor="register-email" className="label-register">Email ID</label>
       <input
         type="text"
+        id="register-email"
         placeholder="Enter mail ID"
         onChange={handleChange}
         name="_id"
         value={temp._id}
       />
+      {errors._id && <p className="error_login">{errors._id}</p>}
+
+      <label htmlFor="register-name" className="label-register">Name</label>
       <input
         type="text"
+        id="register-name"
         placeholder="Enter name"
         onChange={handleChange}
         name="name"
         value={temp.name}
       />
+      {errors.name && <p className="error_login">{errors.name}</p>}
+
+      <label htmlFor="register-password" className="label-register">Password</label>
       <input
         type="password"
+        id="register-password"
         placeholder="Enter password"
         onChange={handleChange}
         name="password"
         value={temp.password}
       />
+      {errors.password && <p className="error_login">{errors.password}</p>}
 
       {message && (
         <div className={isError ? "error-msg" : "success-msg"}>{message}</div>
@@ -62,13 +101,12 @@ const Register = () => {
 
       <p className="login-text">
         Already have an account?{" "}
-        <Link to="/login" className="login-link">
-          Login
-        </Link>
+        <Link to="/login" className="login-link">Login</Link>
       </p>
     </div>
   );
 };
 
-
 export default Register;
+
+
