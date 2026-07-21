408f916bc23a3726c2b40d6f865e868370671621
sridhar reddy <sridharnani080@gmail.com>
2026-07-21 12:29:05 +0530

    build

--- day2-portfolio/src/App.js
+++ day2-portfolio/src/App.js
@@ -2,7 +2,7 @@ import { Header } from "./Navbar/Navbar";
 import Home from "./Home/Home";
 import About from "./About/About";
 import Skills from "./Skills/Skills";
-import Projects from "./Projects/Projects";
+import Projects from "./projects/projects";
 import ContactForm from "./Contact/Contact";
 
 export const App = () => {
